90929ef9bc347f70f49e5bbb9b73f5454bbc9164
ramakrishna67 <ramakrishnapaila64@gmail.com>
2025-08-01 20:00:18 +0530

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default async function HomePage() {
   const res = await fetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false",
     {
-      next: { revalidate: 60 }, // Revalidate every 60 seconds
       cache: "no-store", // Disable caching for this request
     }
   );
